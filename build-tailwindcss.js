cc788770edc56759182603648e2850483a518152
cinos81 <cinos81@gmail.com>
2023-01-28 21:25:20 +0900

    feat: tailwind.config.js 용 트랜스포머를 통합

---
+++ build-tailwindcss.js
@@ -0,0 +1,12 @@
+const StyleDictionaryModule = require('style-dictionary')
+const { makeSdTailwindConfig } = require('sd-tailwindcss-transformer')
+
+const StyleDictionary = StyleDictionaryModule.extend(
+    makeSdTailwindConfig({
+        type: 'all',
+        // source: ['theme/global.json', 'theme/dark.json', 'theme/dark.json']
+        source: ['data/tokens.json']
+    })
+)
+
+StyleDictionary.buildAllPlatforms()
